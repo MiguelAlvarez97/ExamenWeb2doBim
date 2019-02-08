59f6b3258a81b90d8a8ce788fc5eddcf86bdec0c
Edison <eddyferch@hotmail.es>
2019-02-08 11:55:24 -0500

    final

--- examen/src/usuario/dto/usuario-create.dto.ts
+++ examen/src/usuario/dto/usuario-create.dto.ts
@@ -1,11 +1,23 @@
 // usuario-crate.dto.ts
 
-import {IsDate, IsDateString, IsEmail, IsEmpty, IsNotEmpty, IsString, Length} from "class-validator";
+import {
+    IsAlpha,
+    IsDate,
+    IsDateString,
+    IsEmail,
+    IsEmpty,
+    IsNotEmpty,
+    IsString,
+    Length,
+    Matches,
+    MinDate
+} from "class-validator";
 
 export class UsuarioCreateDto {
 
     @IsNotEmpty()
     @IsString()
+    @Matches(/^[a-zA-Z\s]*$/,{message:"Nombre no valido"})
     @Length(3,20)
     nombre:string;
 
@@ -17,6 +29,14 @@ export class UsuarioCreateDto {
 
     @IsNotEmpty()
     @IsString()
+    @Matches( /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*#?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,16}$/ ,
+            {message: "Contrasenia Invalida"
+        })
+
     password:string;
 
+   /* @IsNotEmpty()
+    @MinDate( new Date() , {message: "La Fecha no Puede superar la fecha actual"} )
+    fecha_nacimiento:string;
+*/
 }
